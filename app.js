81f529996bdf8123851d008f33c6ff8e18766817
Carlos Eduardo González Cortes <carlosedugoc@gmail.com>
2018-02-06 13:42:50 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+//Requires
+var express = require('express');
+var mongoose = require('mongoose')
+
+
+//Inicializar variables
+var app = express();
+
+//Conexion a la base de datos
+mongoose.connection.openUri('mongodb://admin:admin@ds125578.mlab.com:25578/hospitaldb',(err, res)=>{
+    if(err) throw err
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online')
+})
+
+
+//Rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        message: 'Petición realizada correctamente'
+    })
+})
+
+//Excuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', 'online')
+})
+
+
+
