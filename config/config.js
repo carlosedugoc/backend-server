514ed677977beec6e5648546c663650ae816f117
Carlos Eduardo González Cortes <carlosedugoc@gmail.com>
2018-02-09 20:02:06 -0500

    Sign in google

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = '@este-es@-un-seed-dificil'
+
+//Google
+
+module.exports.GOOGLE_CLIENT_ID = '949951332702-863ia704hf4hfbi9a5liqo8bu325sn80.apps.googleusercontent.com'
+module.exports.GOOGLE_SECRET = 'q7n8-hzX9L6RaubiIeYMIyeJ'
--- routes/login.js
+++ routes/login.js
@@ -3,16 +3,62 @@ var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
-var SEED =  require('../config/config').SEED
+var SEED = require('../config/config').SEED
 
 var app = express();
 
 var Usuario = require('../models/usuario');
 
-app.post('/', (req, res)=>{
+var GoogleAuth = require('google-auth-library');
+// var auth = new GoogleAuth;
+
+// var auth = new GoogleAuth.OAuth2Client(
+//     config.GOOGLE_CLIENT_ID,
+//     config.GOOGLE_SECRET,
+//     ""
+//     );
+
+var GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID
+var GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET
+
+//Autenticacion GOOGLE
+
+app.post('/google', (req, res) => {
+
+    var token = req.body.token || 'XXX'
+    var client = new GoogleAuth.OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
+
+    client.verifyIdToken({ idToken: token, audience: GOOGLE_CLIENT_ID },
+        function (err, login) {
+            if (err) {
+                return res.status(400).json({
+                    ok: true,
+                    mesaje: 'Token no valido',
+                    errors: err
+                })
+            }
+
+            var payload = login.getPayload();
+            var userid = payload['sub'];
+            // If request specified a G Suite domain:
+            //var domain = payload['hd'];
+
+            res.status(200).json({
+                ok: true,
+                payload
+            })
+
+        }
+    );
+
+})
+
+//Autenticacion Normal
+
+app.post('/', (req, res) => {
     var body = req.body;
 
-    Usuario.findOne({email: body.email}, (err, usuarioDB)=>{
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
 
         if (err) {
             return res.status(500).json({
@@ -22,7 +68,7 @@ app.post('/', (req, res)=>{
             })
         }
 
-        if(!usuarioDB){
+        if (!usuarioDB) {
             return res.status(400).json({
                 ok: false,
                 message: 'Credenciales incorrectas - email',
@@ -30,7 +76,7 @@ app.post('/', (req, res)=>{
             })
         }
 
-        if (!bcrypt.compareSync(body.password, usuarioDB.password)){
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
             return res.status(400).json({
                 ok: false,
                 message: 'Credenciales incorrectas - password',
@@ -40,7 +86,7 @@ app.post('/', (req, res)=>{
 
         //Crear Token
         usuarioDB.password = ':)'
-        var token = jwt.sign({usuario:usuarioDB},SEED,{expiresIn:14400}) //4 horas
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }) //4 horas
 
 
         res.status(200).json({
@@ -51,7 +97,7 @@ app.post('/', (req, res)=>{
         })
     })
 
-    
+
 })
 
 
